882d4722c09e8443fe84b82773e86b137c985723
lucasfog <45837276+Lucasfog@users.noreply.github.com>
2024-09-01 23:51:40 -0300

    Corrigindo problema de vazamento da key da API

--- lib/constants.dart
+++ lib/constants.dart
@@ -1,5 +1,5 @@
 class Constants {
-  static const apiKey = '74ab60838726f35946d1075bf9e82a09';
+  static const apiKey = const String.fromEnvironment('API_KEY');
   static const imagePath = 'https://image.tmdb.org/t/p/w500';
 
   String formatDate(String text) {
